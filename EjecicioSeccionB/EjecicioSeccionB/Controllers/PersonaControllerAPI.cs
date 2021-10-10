756794ef78b449fecd08d6104b44588db0113175
Gustavo Mendez <gmendez.2211@gmail.com>
2021-10-10 08:31:22 -0600

    Se agrega controlador para el manejo de metodos en API.

---
+++ EjecicioSeccionB/EjecicioSeccionB/Controllers/PersonaControllerAPI.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using EjecicioSeccionB.Repository.iRepository;
+using EjecicioSeccionB.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjecicioSeccionB.Controllers
+{
+    [Route("api/Persona")]
+    [ApiController]
+    public class PersonaControllerAPI : Controller
+    {
+        private readonly iPersonaRepository _ctoPersonas;
+        private readonly IMapper _mapper;
+
+        //constructor
+        public PersonaControllerAPI(iPersonaRepository ctoPersonas, IMapper mapper)
+        {
+            _ctoPersonas = ctoPersonas;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ListaPersonas()
+        {
+            var nListaPersona = _ctoPersonas.GetPersonaModels();
+
+            var nListaPersonaDTO = new List<PersonaDto>();
+
+            foreach(var nLista in nListaPersona)
+            {
+                nListaPersonaDTO.Add(_mapper.Map<PersonaDto>(nLista));
+            }
+            return Ok(nListaPersonaDTO);
+        }
+
+    }
+}
